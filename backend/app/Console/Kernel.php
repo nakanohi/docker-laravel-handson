b15f1160db5dab47a676a866fe56c068e21c4699
hiromichi.nakano <nakanohi113@gmail.com>
2021-11-16 20:10:45 +0900

    scraiping

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,6 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
+        Commands\ScrapingCommand::class
         //
     ];
 
@@ -24,7 +26,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        Log::setDefaultDriver('batch');
+        Log::info('OKgo');
+        $schedule->command('writelog:amazon-scraping')->everyMinute();
     }
 
     /**
